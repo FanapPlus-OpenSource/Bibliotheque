6d01b8c06d0b97bcb17e6ec92ffd5303132665bc
Hessam Emami <hesam.emami9@gmail.com>
2020-03-27 17:44:59 +0430

    Added preloaded database insert on app startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+insert into book (author, isbn, title)
+VALUES ('Charles Dickens', 111103213, 'The Dame');
+insert into book (author, isbn, title)
+VALUES ('Dan Brown', 312444123, 'Angels and Demons');
+insert into book (author, isbn, title)
+VALUES ('Dan Brown', 123456987, 'Davinci code');
+insert into book (author, isbn, title)
+VALUES ('Alexander Dumas', 55465487, 'Count of monte christo');
+insert into book (author, isbn, title)
+VALUES ('Jules Vernes', 321546878, 'Natilus');
+insert into user (name, email)
+VALUES ('Hessam', 'hessam@test.com');
+insert into user (name, email)
+VALUES ('John', 'john@test.com');
+
+insert into user_book (book_id, user_id)
+values (1, 1);
+insert into user_book (book_id, user_id)
+values (3, 2);
+insert into user_book (book_id, user_id)
+values (4, 2);
